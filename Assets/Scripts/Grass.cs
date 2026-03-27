55c4977bc7af3a945749c378b55e7f91c9b9ee7d
KelvinOrion <kelvinlee.97@gmail.com>
2026-03-27 18:23:43 +0800

    v1.25 Tree not spawning
    Fixes:
    - Previously it was hardcoded
    - fix to make it updateable through Unity Inspector

--- Assets/Scripts/Grass.cs
+++ Assets/Scripts/Grass.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 public class Grass : MonoBehaviour
 {
     [SerializeField] private Transform treePrefab;
+    
+    // Playable area boundaries - match GameManager's InStartArea bounds
+    // These should match the character's movement constraints
+    [SerializeField] private int playableAreaMinX = -10;
+    [SerializeField] private int playableAreaMaxX = 10;
 
     public HashSet<int> Init(float z)
     {
         // Place the obstacle at the location provided.
         transform.position = new Vector3(0, 0, z);
 
-        // We always have obstacles outside the game area.
-        HashSet<int> locations = new() { -10, 10 };
+        // We always have obstacles at the edges of the playable area.
+        HashSet<int> locations = new() { playableAreaMinX, playableAreaMaxX };
 
-        // Populate with some obstacles
+        // Populate with some obstacles within the playable area
         int numTrees = Random.Range(1, 5);
 
         for (int i = 0; i < numTrees; i++)
@@ -21,8 +26,8 @@ public class Grass : MonoBehaviour
             // Create a new tree object
             Transform tree = Instantiate(treePrefab, transform);
 
-            // Put it in a random position
-            int xPos = Random.Range(-5, 6);
+            // Put it in a random position within playable area (excluding edges)
+            int xPos = Random.Range(playableAreaMinX + 1, playableAreaMaxX);
             tree.position = new Vector3(xPos, 0.2f, z);
 
             // Record the location in our HashSet.
